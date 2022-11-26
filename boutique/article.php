f1d0331c231e617446ac8fcd800514461d4feb6d
mylanlegenie <mylanhachemane@gmail.com>
2022-11-26 12:55:28 +0100

    Page article V1

--- boutique/article.php
+++ boutique/article.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <link rel="stylesheet" href="/css/article.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +10,22 @@
 <body>
     <?php include '../menu.php'; ?>
     <div id="article_box">
-        <img src="/images/boutique/sourisg502.png">
+        <div id="img_article" >
+            <img src="/images/boutique/clavierg915tkl.png">
+        </div>
+        <div id="infos_article">
+            <div id="infos_produits">
+                <h2>SOURIS LOGITECH G502</h2><br>
+                <p>149.00€</p>
+                <p>Stock : 3</p>
+                <p>Conçue pour des performances de jeu avancées. La souris G502 HERO est dotée du capteur gaming HERO 25K avec un suivi de précision au sous-micron près, un éclairage LIGHTSYNC RVB personnalisable, des profils intégrés, des poids amovibles et bien plus encore.</p>
+            </div>
+            <div id="ajout_panier">
+                <button onclick="ajout_panier();">Ajouter au panier</button>
+                <p>Vendu par Mylan</p>
+            </div>
+            
+        </div>
 
 
 
